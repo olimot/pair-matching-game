960df0f2803e2e3d75a692b2f23025b863cc87cc
olimot <olimot@gmail.com>
2022-11-28 17:18:23 +0900

    수정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const colorSet = ['red', 'gold', 'green', 'blue', 'purple', 'gray', 'black'] as const;
-const typeSet = ['♠', '♥', '♦', '♣', '♚', '♛', '☻'] as const;
+const typeSet = ['♚', '♛', '♜', '♝', '♞', '♟︎', '☻'] as const;
 
 interface Combination {
   color: typeof colorSet[number];
-  type: typeof typeSet[number] | '☀';
+  type: typeof typeSet[number] | '☙';
 }
 
 interface Cell {
@@ -28,14 +28,9 @@ const randMultInt = (mult: number) => (Math.random() * mult) | 0;
 
 const generateGame = (): GameState => {
   const slots = Array.from(Array(100), (_, i) => ({ index: i, row: Math.floor(i / 10), col: i % 10 }));
-  const combinations: Combination[] = Array.from(Array(49), (_, i) => ({
-    type: typeSet[Math.floor(i / 7)],
-    color: colorSet[i % 7],
-  }));
-  combinations.push({ type: '☀', color: colorSet[randMultInt(7)] });
   const cells: BoardCell[] = [];
   for (let i = 0; i < 50; i += 1) {
-    const type = i === 49 ? '☀' : typeSet[Math.floor(i / 7)];
+    const type = i === 49 ? '☙' : typeSet[Math.floor(i / 7)];
     const color = i === 49 ? colorSet[randMultInt(7)] : colorSet[i % 7];
     const [pair1Slot] = slots.splice(randMultInt(slots.length), 1);
     const [pair2Slot] = slots.splice(randMultInt(slots.length), 1);
@@ -71,19 +66,19 @@ const App = () => {
       cancelAnimationFrame(timerId);
     };
   }, [targetTime, gameState]);
-
+  const score = Object.keys(solved).length;
   return (
     <div>
       <h1>짝 맞추기 게임!</h1>
       <div className="i-enjoy">즐겁다</div>
       <h2>
-        Score: {Object.keys(solved).length}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Time Left:{' '}
-        {timeLeft.toFixed(3)}ms
+        Score: {score}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Time Left: {timeLeft.toFixed(3)}ms
       </h2>
       {!gameState || timeLeft <= 0 ? (
         <div className="game-scene">
           <button
             type="button"
+            className="play"
             onClick={() => {
               setTargetTime(30000);
               setSelected(null);
@@ -96,36 +91,62 @@ const App = () => {
         </div>
       ) : (
         <div className="game-scene">
-          <div className="time-gauge-bar">
-            <div
-              className="time-gauge"
-              style={{ width: `${Math.min(Math.max(0, 100 * (timeLeft / targetTime)), 100)}%` }}
-            />
-          </div>
-          <div className="board">
-            {gameState.cells.map(({ index, type, color, isFake }) => (
+          {score === 100 ? (
+            <>
+              <h1>클리어!</h1>
               <button
-                key={index}
-                className={selected === index ? 'cell cell--selected' : solved[index] ? 'cell cell--solved' : 'cell'}
+                type="button"
+                className="play"
                 onClick={() => {
-                  if (selected === null) {
-                    setSelected(index);
-                    return;
-                  }
-                  if (
-                    (gameState.cells[selected].isFake && isFake) ||
-                    (gameState.cells[selected].type === type && gameState.cells[selected].color === color)
-                  ) {
-                    setSolved((prev) => ({ ...prev, [index]: true, [selected]: true }));
-                    setTargetTime((t) => t + 1000);
-                  }
+                  setTargetTime(30000);
                   setSelected(null);
+                  setSolved({});
+                  setGameState(generateGame);
                 }}
               >
-                <span style={{ color }}>{type}</span>
+                다시하기
               </button>
-            ))}
-          </div>
+            </>
+          ) : (
+            <>
+              <div className="time-gauge-bar">
+                <div
+                  className="time-gauge"
+                  style={{ width: `${Math.min(Math.max(0, 100 * (timeLeft / targetTime)), 100)}%` }}
+                />
+              </div>
+              <div className="board">
+                {gameState.cells.map(({ index, type, color, isFake }) => (
+                  <button
+                    key={index}
+                    className={
+                      selected === index ? 'cell cell--selected' : solved[index] ? 'cell cell--solved' : 'cell'
+                    }
+                    onClick={() => {
+                      if (selected === null) {
+                        setSelected(index);
+                        return;
+                      }
+                      if (selected === index) {
+                        setSelected(null);
+                        return;
+                      }
+                      if (
+                        (gameState.cells[selected].isFake && isFake) ||
+                        (gameState.cells[selected].type === type && gameState.cells[selected].color === color)
+                      ) {
+                        setSolved((prev) => ({ ...prev, [index]: true, [selected]: true }));
+                        setTargetTime((t) => t + 1000);
+                      }
+                      setSelected(null);
+                    }}
+                  >
+                    <span style={{ color }}>{type}</span>
+                  </button>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       )}
     </div>
